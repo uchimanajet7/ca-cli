4f58ce6d8dd893dd06714b9a4b6c2e02ee9cc3f9
uchimanajet7 <uchimanajet7@gmail.com>
2017-08-06 17:58:17 +0900

    create job create helper

---
+++ cmd/job_create_helper.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+)
+
+// ca job rule_type
+type jobRuleType struct {
+	Name        string
+	IsRuleValue bool
+}
+
+// see also:
+// https://cloudautomator.com/api_docs/v1/api.html#header--1
+var jobRuleTypeList = []jobRuleType{
+	jobRuleType{
+		Name:        "cron",
+		IsRuleValue: true,
+	},
+	jobRuleType{
+		Name:        "immediate_execution",
+		IsRuleValue: false,
+	},
+	jobRuleType{
+		Name:        "webhook",
+		IsRuleValue: false,
+	},
+	jobRuleType{
+		Name:        "sqs",
+		IsRuleValue: true,
+	},
+	jobRuleType{
+		Name:        "amazon_snssqs",
+		IsRuleValue: false,
+	},
+}
+
+func isValidJobRuleType(name string) (bool, bool) {
+	for _, v := range jobRuleTypeList {
+		if strings.ToLower(name) == strings.ToLower(v.Name) {
+			return true, v.IsRuleValue
+		}
+	}
+	return false, false
+}
+
+// see also:
+// https://cloudautomator.com/api_docs/v1/api.html#header--2
+func createJobRuleValueParameter(ruleName string, raw string) (*map[string]interface{}, error) {
+	// parse raw string
+	rawSlice := strings.Split(raw, ",")
+	parsedMap := make(map[string]interface{}, len(rawSlice))
+	prevKey := ""
+	for _, v := range rawSlice {
+
+		if strings.Index(v, "=") > 0 {
+			slice := strings.Split(v, "=")
+
+			if len(slice) < 2 {
+				return nil, errors.New("\nPlease specify flag [--rule-type] required CA job trigger type.")
+			}
+			mapKey := strings.TrimSpace(slice[0])
+			mapVal := strings.TrimSpace(slice[1])
+			parsedMap[mapKey] = mapVal
+			prevKey = mapKey
+			continue
+		}
+
+		// not exsit "="
+		prevVal := parsedMap[prevKey]
+		if reflect.TypeOf(prevVal).Name() == "string" {
+			prevVal = []string{prevVal.(string)}
+		}
+		parsedMap[prevKey] = append(prevVal.([]string), strings.TrimSpace(v))
+	}
+
+	return &parsedMap, nil
+}
